701530a39b31348d93e06c6b867d081827f501ee
Farris <farrisessam@protonmail.com>
2020-04-25 18:01:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         description='A simple CLI program that highlights rhymes in a given text',
         author='Farris Essam',
         url='https://github.com/dimeritium-foil/rhymepy',
-        install_requires=['colored'],
+        install_requires=['colored', 'requests'],
         extras_require={'Native rhymes matching via the pronouncing module.': ['pronouncing']},
         packages=['rhymepy'],
         package_data={'': ['default_rhymepy.ini']},
