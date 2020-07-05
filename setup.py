ad72cc275f4563e68782d33972be049ad1dfe811
dimeritium-foil <farrisessam@protonmail.com>
2020-07-05 17:40:04 +0200

    Updated the setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 #!/bin/env python
 
+from pathlib import Path
 from setuptools import setup
+
+# The directory containing this file
+HERE = Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setup(
         name='rhymepy',
-        version='0.1',
+        version='0.1.0',
         description='A simple CLI program that highlights rhymes in a given text',
+        long_description=README,
+        long_description_content_type="text/markdown",
         author='Farris Essam',
         url='https://github.com/dimeritium-foil/rhymepy',
         install_requires=['colored', 'requests'],
